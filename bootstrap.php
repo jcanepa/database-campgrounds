024b7d60b90b5fcbcb73567024e9e4e3720d5b4e
Julian Canepa <julian.l.canepa@gmail.com>
2022-05-26 12:45:39 -0700

    Move order of operations in bootstrap file, only create database objects once the query has been deemed valid.

--- bootstrap.php
+++ bootstrap.php
@@ -5,17 +5,17 @@ require 'vendor/autoload.php';
 use Jcanepa\DatabaseCampgrounds\Connection;
 use Jcanepa\DatabaseCampgrounds\QueryBuilder;
 
-$config = require 'config.php';
-
-$db = new QueryBuilder(
-        Connection::make($config['database']));
-
 /** Accept incoming json request data from vue */
 $query = json_decode(
     file_get_contents('php://input')
     )->sql_statement;
 
 if (!$query) return [];
+
+$config = require 'config.php';
+
+$db = new QueryBuilder(
+        Connection::make($config['database']));
 
 echo json_encode(
     $db->run($query));
