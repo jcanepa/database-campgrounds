e346a8e08c64240815f8d44923158c3c6b6f25e4
Julian Canepa <julian.l.canepa@gmail.com>
2022-06-07 23:49:20 -0700

    Add favicon to clear console error.

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NPS Campgrounds</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
     <style>
         dl:nth-child(even) {background-color: rgb(248 250 252)}
         dl:nth-child(odd) {background-color: rgb(226 232 240)}
